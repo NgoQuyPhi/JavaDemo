6a5f3282d4b733c88ee8a11eb58901a6797d123b
NgoQuyPhi <ngoquyphitn@gmail.com>
2023-03-02 15:23:58 +0700

    second commit

---
+++ JavaDemo/BaiThucHanh0302/src/Slide55.java
@@ -0,0 +1,16 @@
+/**
+ * Slide55
+ */
+public class Slide55 {
+
+    public static void main(String[] args) {
+        int number=0;
+        for(;number<200;){
+            if(number%5 == 0){
+                System.out.println(number);
+                number+=5;
+            }
+        }
+
+    }
+}
